7f1158585bc2cb9de7071bc75e2f725875c46220
kthong3 <kthongsakounh@gmail.com>
2018-11-16 10:17:01 -0600

    refactor

--- goodreads_test.rb
+++ goodreads_test.rb
@@ -1,7 +1,7 @@
 require 'test/unit'
 require 'selenium-webdriver'
 
-class LoginTest < Test::Unit::TestCase
+class GoodreadsTest < Test::Unit::TestCase
   def setup
     # define driver for firefox webdriver
     @driver=Selenium::WebDriver.for :chrome
@@ -12,7 +12,7 @@ class LoginTest < Test::Unit::TestCase
     @driver.navigate.to "https://www.goodreads.com"
   end
 
-  def test_login
+  def login_user
     # define username field element
     loginUserName=@driver.find_element(:id,'userSignInFormEmail')
     # input user name
@@ -31,6 +31,10 @@ class LoginTest < Test::Unit::TestCase
     # wait until 'Currently Reading' displays, timeout in 10 seconds
     wait = Selenium::WebDriver::Wait.new(:timeout => 10) # seconds
     element = wait.until { @driver.find_element(css: 'h3') }
+  end
+
+  def test_login
+    login_user
 
     # define currently reading header
     currently_reading=@driver.find_element(css: 'h3')
@@ -43,6 +47,36 @@ class LoginTest < Test::Unit::TestCase
     assert_equal(true, is_currently_reading_displayed,'currently reading display')
 
     assert_equal("currently reading".upcase, currently_reading.text, 'currently reading text')
+  end
+
+  def test_search
+    login_user
+
+    # define search bar field element
+    search_bar=@driver.find_element(:xpath,'/html/body/div[3]/div/header/div[2]/div/div[3]/form/input')
+    # input partial book title
+    search_bar.send_keys('harry potter', :return)
+
+  #   # #define submit button element
+  #   # search_bar_button=@driver.find_element(:css,'searchBox__icon--magnifyingGlass.gr-iconButton searchBox__icon.searchBox__icon--navbar')
+  #   # #click on submit button
+  #   # search_bar_button.click
+
+  #   # wait until 'Currently Reading' displays, timeout in 10 seconds
+  #   # wait = Selenium::WebDriver::Wait.new(:timeout => 10) # seconds
+  #   # element = wait.until { @driver.find_element(css: 'h3') }
+
+  #   # # define currently reading header
+  #   # currently_reading=@driver.find_element(css: 'h3')
+
+  #   # # check if currently reading header is displayed
+  #   # is_currently_reading_displayed=currently_reading.displayed?
+
+  #   # puts is_currently_reading_displayed
+
+  #   # assert_equal(true, is_currently_reading_displayed,'currently reading display')
+
+  #   # assert_equal("currently reading".upcase, currently_reading.text, 'currently reading text')
   end
 
   def teardown
